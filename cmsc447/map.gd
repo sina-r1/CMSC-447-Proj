8d1f308933841550db710c898862942fc463d959
BE15139 <damong1@umbc.edu>
2025-09-22 23:07:30 -0400

    A map image has been added. Touch controls have been implemented to zoom and move the map around, but they need refinement.

---
+++ cmsc447/map.gd
@@ -0,0 +1,37 @@
+extends Node2D
+
+@onready var map_sprite = $"Map Sprite"
+@export var map_image_path = "res://2024-CAMPUS-MAP-2.jpg"
+
+@onready var background_color = $"Background Color"
+
+var touch_inputs = {}
+
+func _ready():
+	var map_image = load(map_image_path)
+	map_sprite.texture = map_image
+	
+	var resolution = get_viewport_rect().size
+	
+	var scale_factor = resolution.x / map_image.get_size().x
+	map_sprite.position = resolution / 2
+	map_sprite.scale = Vector2(scale_factor, scale_factor)
+	
+	background_color.size = resolution
+
+
+func _input(event):
+	if event is InputEventScreenTouch and event.is_released():
+		touch_inputs.erase(event.index)
+	elif event is InputEventScreenTouch or event is InputEventScreenDrag:
+		touch_inputs[event.index] = event
+		if event is InputEventScreenDrag:
+			map_sprite.position += event.relative
+		
+	if (touch_inputs.has(0) 
+	and touch_inputs.has(1)
+	and touch_inputs[0] is InputEventScreenDrag
+	and touch_inputs[1] is InputEventScreenDrag):
+		var current_distance = (touch_inputs[0].position - touch_inputs[1].position).length()
+		var previous_distance = ((touch_inputs[0].position - touch_inputs[0].relative) - (touch_inputs[1].position - touch_inputs[1].relative)).length()
+		map_sprite.scale *= (current_distance / previous_distance)
