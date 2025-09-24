e86a61cf1cc914dedd948f9ebc462d89f2ef4c1e
BE15139 <damong1@umbc.edu>
2025-09-23 21:28:31 -0400

    Touch controls to scroll and zoom the map have been implemented.

--- cmsc447/map.gd
+++ cmsc447/map.gd
@@ -7,17 +7,21 @@ extends Node2D
 
 var touch_inputs = {}
 
+var screen_res
+var initial_scale_factor
+@export var max_zoom_factor = 20
+
 func _ready():
 	var map_image = load(map_image_path)
 	map_sprite.texture = map_image
 	
-	var resolution = get_viewport_rect().size
+	screen_res = get_viewport_rect().size
 	
-	var scale_factor = resolution.x / map_image.get_size().x
-	map_sprite.position = resolution / 2
-	map_sprite.scale = Vector2(scale_factor, scale_factor)
+	initial_scale_factor = screen_res.x / map_image.get_size().x
+	map_sprite.position = screen_res / 2
+	map_sprite.scale = Vector2(initial_scale_factor, initial_scale_factor)
 	
-	background_color.size = resolution
+	background_color.size = screen_res
 
 
 func _input(event):
@@ -35,3 +39,24 @@ func _input(event):
 		var current_distance = (touch_inputs[0].position - touch_inputs[1].position).length()
 		var previous_distance = ((touch_inputs[0].position - touch_inputs[0].relative) - (touch_inputs[1].position - touch_inputs[1].relative)).length()
 		map_sprite.scale *= (current_distance / previous_distance)
+		if map_sprite.scale.x < initial_scale_factor * max_zoom_factor and map_sprite.scale.y < initial_scale_factor * max_zoom_factor:
+			var pinch_center = (touch_inputs[0].position + touch_inputs[1].position) / 2
+			map_sprite.position = pinch_center + (map_sprite.position - pinch_center) * (current_distance / previous_distance)
+		
+	clamp_scale()
+	clamp_position()
+
+func clamp_scale():
+	if map_sprite.scale.x < initial_scale_factor:
+		map_sprite.scale *= initial_scale_factor / map_sprite.scale.x
+	if map_sprite.scale.y < initial_scale_factor:
+		map_sprite.scale *= initial_scale_factor / map_sprite.scale.y
+	if map_sprite.scale.x > initial_scale_factor * max_zoom_factor:
+		map_sprite.scale *= (initial_scale_factor * max_zoom_factor) / map_sprite.scale.x
+	if map_sprite.scale.y > initial_scale_factor * max_zoom_factor:
+		map_sprite.scale *= (initial_scale_factor * max_zoom_factor) / map_sprite.scale.y
+
+func clamp_position():
+	var image_res = map_sprite.texture.get_size()
+	map_sprite.position.x = clampf(map_sprite.position.x, (screen_res.x-image_res.x*map_sprite.scale.x)/2, (screen_res.x+image_res.x*map_sprite.scale.x)/2)
+	map_sprite.position.y = clampf(map_sprite.position.y, (screen_res.y-image_res.y*map_sprite.scale.y)/2, (screen_res.y+image_res.y*map_sprite.scale.y)/2)
